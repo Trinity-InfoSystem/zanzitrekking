bf32b7837cb6ba88d8df5bc66d297c47fd5ccb92
ali aoua <aouaali97@gmail.com>
2026-03-04 22:04:52 +0100

    Remove legacy Product model and all related code
    - Deleted Product model file (models/product.js)
    - Deleted Product controller (controllers/dashboard/productController.js)
    - Deleted Product routes (routes/dashboard/productRoutes.js)
    - Deleted QueryProducts utility (utilities/queryProducts.js)
    - Removed Product-related methods from homeController (get_products, price_range_product, query_products, product_details, formateProduct)
    - Removed Product-related routes from homeRoutes
    - Removed Product route registration from server.js

--- zanzitrekking-backend/routes/home/homeRoutes.js
+++ zanzitrekking-backend/routes/home/homeRoutes.js
@@ -7,10 +7,6 @@ const { validate } = require("../../middlewares/validationMiddleware");
 const { contactSchema } = require("../../validators/contactValidation");
 
 router.get("/get-categories", homeController.get_catgories);
-router.get("/get-products", homeController.get_products);
-router.get("/price-range-latest-product", homeController.price_range_product);
-router.get("/query-products", homeController.query_products);
-router.get("/product-details/:productId", homeController.product_details);
 router.get("/get-statistic-data", whoWeAreController.get_statistic_data);
 router.post("/contact", validate(contactSchema), contactController.send_contact_message);
 
